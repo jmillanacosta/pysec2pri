341812a887bd7ba8145e2480bc0e961b5920ab1d
Javier <javier.millanacosta@maastrichtuniversity.nl>
2026-06-12 15:31:09 +0200

    Bump version: 0.0.14 → 0.0.15-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = f"{date.today().year}, Javier Millán Acosta"
 author = "Javier Millán Acosta"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.0.14"
+release = "0.0.15-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/pysec2pri/version.py
+++ src/pysec2pri/version.py
@@ -12,7 +12,7 @@ __all__ = [
     "get_version",
 ]
 
-VERSION = "0.0.14"
+VERSION = "0.0.15-dev"
 
 
 def get_git_hash() -> str:
