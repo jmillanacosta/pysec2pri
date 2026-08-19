f9371eca616f5bd7126b4e37f3eb4b79fac533b6
Javier <javier.millanacosta@maastrichtuniversity.nl>
2026-08-19 10:32:15 +0200

    Bump to 4.0.0-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = f"{date.today().year}, Javier Millán Acosta"
 author = "Javier Millán Acosta"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.3.12"
+release = "0.4.0-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
